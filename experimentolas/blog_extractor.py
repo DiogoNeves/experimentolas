e04a5ad2fde716088fb182118a20943d1c7d29ab
Diogo Neves <diogo.neves@gmail.com>
2014-12-17 00:56:21 +0000

    Passes all tests and basic implementation
    Needs cleaning

--- experimentolas/blog_extractor.py
+++ experimentolas/blog_extractor.py
@@ -6,6 +6,8 @@
 from collections import namedtuple
 import itertools
 import rfc3987 as iri
+import HTMLParser
+from bs4 import BeautifulSoup
 
 
 Blog = namedtuple('Blog', ['title', 'subtitle', 'url', 'posts'])
@@ -23,32 +25,75 @@ def _is_valid_url(url):
     return iri_match is not None
 
 
-def get_blog_data_from(url, page_requester):
+def get_blog_data_from(url, page_requester, page_limit):
+    def get_parser(html):
+        return BeautifulSoup(html)
+
     if not url:
         return empty_blog
     if not _is_valid_url(url):
         raise BlogException('Invalid blog url "%s"' % url)
 
+    page_parser = None
+    posts = []
+    for page_url in find_all_page_urls(url, page_limit):
+        page_html = page_requester(page_url)
+        if page_html:
+            page_parser = get_parser(page_html)
+            posts.extend(get_all_post_data_from(page_parser))
+        else:
+            break
 
-def find_all_page_urls(base_url):
+    if page_parser:
+        title = page_parser.find('h1', 'site-title').text
+    return Blog(title=title or '', subtitle='', url=url, posts=posts)
+
+
+def find_all_page_urls(base_url, page_limit):
     def form_page_url(page):
         prefix_slash = '' if base_url.endswith('/') else '/'
         return '%s%spage/%d' % (base_url, prefix_slash, page)
 
     assert _is_valid_url(base_url)
-    return (form_page_url(i) for i in itertools.count(1))
+    assert page_limit > 0
+    return (form_page_url(i) for i in xrange(1, page_limit + 1))
 
 
-def get_all_post_data_from(page_html):
-    return []
+def get_all_post_data_from(page_parser):
+    def get_content_parser():
+        return page_parser.find('main') or BeautifulSoup('')
+
+    content_parser = get_content_parser()
+    posts = find_all_posts(content_parser)
+    return [try_get_post_data_from(post) for post in posts]
 
 
-def find_all_posts(page_parser):
-    return (i for i in xrange(0))
+def find_all_posts(content_parser):
+    post_tag = 'article'
+    current_post = content_parser.find(post_tag)
+    while current_post is not None:
+        yield current_post
+        current_post = current_post.find_next(post_tag)
 
 
-def get_post_data_from(post_parser):
-    return empty_post
+def try_get_post_data_from(post_parser):
+    try:
+        return _get_post_data_from(post_parser)
+    except (KeyError, HTMLParser.HTMLParseError):
+        return empty_post
+
+
+def _get_post_data_from(post_parser):
+    post_id = post_parser['id']
+    header = post_parser.find('header')
+    title = header.find('a').text
+    image = header.find('img')
+    if image and 'src' in image.attrs:
+        image = image['src']
+    content_parser = post_parser.find('div', 'entry-content')
+    content = ''.join(map(str, content_parser.children))
+    return Post(id=post_id, title=title, subtitle='', image_url=image or '',
+                content=content)
 
 
 def main():
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,14 +5,14 @@ from bs4 import BeautifulSoup
 from experimentolas.blog_extractor import Post, Blog, BlogException, \
     empty_blog, empty_post
 from experimentolas.blog_extractor import get_blog_data_from, \
-    find_all_page_urls, get_all_post_data_from, find_all_posts, get_post_data_from
+    find_all_page_urls, get_all_post_data_from, find_all_posts, \
+    try_get_post_data_from
 
 
-page_html = '<html><body>%s</body></html>'
+page_html = '<html><body><h1 class="site-title">Some Blog</h1>%s</body></html>'
 
 
-single_post_html = """<main id="main">
-<article id="post-001">
+single_post_html = """<article id="post-001">
     <header>
         <h1 class="entry-title">
             <a href="some-link">Some Post</a>
@@ -23,11 +23,12 @@ single_post_html = """<main id="main">
             </a>
         </div>
     </header>
-    <div class="entry-content">
-        <p>Some text.</p>
-    </div>
+    <div class="entry-content"><p>Some text.</p></div>
 </article>
-</main>"""
+"""
+
+
+single_post_page_html = '<main>%s</main>' % single_post_html
 
 
 single_post_data = Post(id='post-001', title='Some Post', subtitle='',
@@ -37,6 +38,9 @@ single_post_data = Post(id='post-001', title='Some Post', subtitle='',
 
 single_blog_data = Blog(title='Some Blog', subtitle='',
                         url='http://someblog.net', posts=[single_post_data])
+
+
+empty_parser = BeautifulSoup('')
 
 
 def mock_page_requester_wrapper(content):
@@ -53,20 +57,20 @@ def assert_no_more_items(iterator):
 
 
 def test_empty_blog_url_has_no_data():
-    assert get_blog_data_from('', null_requester) == empty_blog
+    assert get_blog_data_from('', null_requester, 10) == empty_blog
 
 
 def test_invalid_url_raises():
     invalid_url = 'httasdf'
     with pytest.raises(BlogException) as ex_info:
-        get_blog_data_from(invalid_url, null_requester)
+        get_blog_data_from(invalid_url, null_requester, 10)
 
     assert ex_info.value.message == 'Invalid blog url "%s"' % invalid_url
 
 
 def test_returns_data():
-    requester = mock_page_requester_wrapper(single_post_html)
-    blog = get_blog_data_from('http://someblog.net', requester)
+    requester = mock_page_requester_wrapper(single_post_page_html)
+    blog = get_blog_data_from('http://someblog.net', requester, 1)
     assert blog == single_blog_data
 
 
@@ -74,7 +78,7 @@ def test_iterator_increments(blog_url='http://someblog.net'):
     def is_valid_page_url(url, page):
         return url.endswith('/page/%d' % page) and url.find('//page') == -1
 
-    page_it = find_all_page_urls(blog_url)
+    page_it = find_all_page_urls(blog_url, 10)
     assert is_valid_page_url(next(page_it), 1)
     assert is_valid_page_url(next(page_it), 2)
 
@@ -83,38 +87,50 @@ def test_trailing_slash_works():
     test_iterator_increments(blog_url='http://someblog.net/')
 
 
+def test_iterator_stops():
+    page_it = find_all_page_urls('http://someblog.net', 1)
+    next(page_it)
+    assert_no_more_items(page_it)
+
+
+def test_iterator_asserts_less_than_one():
+    with pytest.raises(AssertionError):
+        find_all_page_urls(base_url='http://someblog.net', page_limit=0)
+
+
 def test_empty_html_has_no_posts():
-    assert get_all_post_data_from('') == []
+    assert get_all_post_data_from(empty_parser) == []
 
 
 def test_single_post_in_page():
-    assert get_all_post_data_from(single_post_html) == [single_post_data]
+    requester = mock_page_requester_wrapper(single_post_page_html)
+    page_parser = BeautifulSoup(requester('http://someblog.net'))
+    assert get_all_post_data_from(page_parser) == [single_post_data]
 
 
 def test_no_posts_in_empty_parser():
-    empty_parser = BeautifulSoup('')
     assert_no_more_items(find_all_posts(empty_parser))
 
 
 def test_single_post_returns_only_one():
-    requester = mock_page_requester_wrapper(single_post_html)
-    single_post_parser = BeautifulSoup(requester('http://someblog.net'))
+    single_post_parser = BeautifulSoup(single_post_page_html)
     post_it = find_all_posts(single_post_parser)
     next(post_it)
     assert_no_more_items(post_it)
 
 
-def test_single_post_returns_data():
-    requester = mock_page_requester_wrapper(single_post_html)
-    single_post_parser = BeautifulSoup(requester('http://someblog.net'))
-    assert next(find_all_posts(single_post_parser)) == single_post_data
+def test_single_post_is_found():
+    single_post_parser = BeautifulSoup(single_post_page_html)
+    assert next(find_all_posts(single_post_parser)) == \
+        BeautifulSoup(single_post_html).find('article')
 
 
 def test_no_data_in_empty_post():
     empty_parser = BeautifulSoup('')
-    assert get_post_data_from(empty_parser) == empty_post
+    assert try_get_post_data_from(empty_parser) == empty_post
 
 
 def test_single_post_data():
     single_post_parser = BeautifulSoup(single_post_html)
-    assert get_post_data_from(single_post_parser) == single_post_data
+    post = single_post_parser.find('article')
+    assert try_get_post_data_from(post) == single_post_data
